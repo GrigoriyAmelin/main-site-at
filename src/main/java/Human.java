3f79330b95f53491de2ecf79eaf83924b5fa1229
Grigoriy Amelin <g-amelin@mail.ru>
2020-05-24 19:01:26 +0300

    Задача 1

---
+++ src/main/java/Human.java
@@ -0,0 +1,21 @@
+public class Human {
+    public String name;
+    public int distanceRun;
+    public double jumpHeight;
+
+    public Human (String name, int distanceRun, double jumpHeight) {
+        this.name = name;
+        this.distanceRun = distanceRun;
+        this.jumpHeight = jumpHeight;
+    }
+
+    public void run () {
+        System.out.println("Human " + name + " can run on " + distanceRun + " kilometers!");
+    }
+
+    public void jump () {
+        System.out.println("Human " + name + " can jump on " + jumpHeight + " meters! You know!");
+        System.out.println();
+    }
+
+}
