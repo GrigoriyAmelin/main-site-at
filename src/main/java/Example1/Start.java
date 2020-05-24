5f373d1b2481936defa36d9a38cdfc70719a18a5
Grigoriy Amelin <g-amelin@mail.ru>
2020-05-25 01:12:07 +0300

    Созданы новые объекты классов в методе main

--- src/main/java/Example1/Start.java
+++ src/main/java/Example1/Start.java
@@ -3,17 +3,26 @@ package Example1;
 public class Start {
     public static void main(String[] args) {
         Cat cat1 = new Cat("Vasya", 2, 1.8);
-        Human human1 = new Human("Kosta", 20, 1.5);
+        Cat cat2 = new Cat("Zheka", 3, 1.6);
+        Human human1 = new Human("Kostya", 20, 1.5);
+        Human human2 = new Human("Abdullah", 42, 1.2);
         Robot robot1 = new Robot("Verter", 100, 1.9);
+        Robot robot2 = new Robot("Basya", 120, 2.1);
 
         cat1.run();
         cat1.jump();
+        cat2.run();
+        cat2.jump();
 
         human1.run();
         human1.jump();
+        human2.run();
+        human2.jump();
 
         robot1.run();
         robot1.jump();
+        robot2.run();
+        robot2.jump();
         System.out.println("It's a wonderful world!!!");
     }
 
