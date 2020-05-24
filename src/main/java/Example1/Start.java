dd37eea8f85695b60b3a76c7a00db1c9a8596b45
Grigoriy Amelin <g-amelin@mail.ru>
2020-05-24 22:44:28 +0300

    Перемещение файлов в ветку lesson_1_hw

--- src/main/java/Example1/Start.java
+++ src/main/java/Example1/Start.java
@@ -4,8 +4,7 @@ public class Start {
     public static void main(String[] args) {
         Cat cat1 = new Cat("Vasya", 2, 1.8);
         Human human1 = new Human("Kosta", 20, 1.5);
-        Robot robot1;
-        robot1 = new Robot("Verter", 100, 1.9);
+        Robot robot1 = new Robot("Verter", 100, 1.9);
 
         cat1.run();
         cat1.jump();
